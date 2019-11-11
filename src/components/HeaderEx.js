f9881b3f9306d311a41bb85cd12dc1a8c4dc5623
tomtsutom <tomtsutom0122@gmail.com>
2019-11-11 18:13:21 +0900

    コードの不要な部分を削除

--- src/components/HeaderEx.js
+++ src/components/HeaderEx.js
@@ -16,30 +16,6 @@ const styles = ({ spacing, transitions, breakpoints, palette, shape }) => ({
   grow: {
     flexGrow: 1
   },
-  search: {
-    position: "relative",
-    marginRight: 8,
-    borderRadius: shape.borderRadius,
-    background: palette.grey[200],
-    "&:hover": {
-      background: palette.grey[300]
-    },
-    marginLeft: 0,
-    width: "100%",
-    [breakpoints.up("sm")]: {
-      marginLeft: spacing(1),
-      width: "auto"
-    }
-  },
-  searchIcon: {
-    width: spacing(9),
-    height: "100%",
-    position: "absolute",
-    pointerEvents: "none",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center"
-  },
   inputRoot: {
     color: "inherit",
     width: "100%"
