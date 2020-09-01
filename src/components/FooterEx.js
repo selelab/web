ad7f4706d4a1a48acc0dec09476ac0ff9926fd8a
shiroza <nemonora-weed37@outlook.jp>
2020-09-01 23:53:21 +0900

    トップページ改修

--- src/components/FooterEx.js
+++ src/components/FooterEx.js
@@ -3,7 +3,7 @@ import Typography from "@material-ui/core/Typography";
 import Divider from "@material-ui/core/Divider";
 
 const FooterEx = () => (
-  <div style={{ maxWidth: 700, margin: "auto", textAlign: "center" }}>
+  <div style={{ maxWidth: 700, margin: "auto", textAlign: "center", color:"#fff",}}>
     <Typography variant="caption" align={"center"}>
       © Copyright 2019
     </Typography>
--- src/contents/Top.js
+++ src/contents/Top.js
@@ -1,6 +1,11 @@
-import Typography from "@material-ui/core/Typography";
 import { withStyles } from "@material-ui/core/styles";
-import React from "react";
+import React from 'react';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
 
 const styles = ({ breakpoints }) => ({
   root: {
@@ -12,21 +17,132 @@ const styles = ({ breakpoints }) => ({
     },
     [breakpoints.up("md")]: {
       maxWidth: 700
+    },
+    color: "#fff",
+  },
+  textGreen: {
+    color: '#92D050',
+  },
+  container: {
+    margin: '20px 0px 20px 0px',
+  },
+  indexImage: {
+    height:130.
+  },
+  media: {
+    height: 200,
+  },
+  cardArea: {
+    display: 'flex',
+    justifyContent: 'center',
+    flexDirection: 'column',
+    alignItems: 'center',
+    [breakpoints.up("md")]: {
+      flexDirection: 'row',
     }
-  }
+  },
+  card: {
+    width: 320,
+    margin: 20,
+    backgroundColor: '#444',
+  },
+  arrow: {
+    position: 'absolute',
+    top: 230,
+    right: 10,
+    color: '#92D050',
+  },
+  cardSubText: {
+    padding: '2px 0px 2px 8px',
+    borderLeft: '3px solid #92D050',
+  },
 });
 
 const Top = ({ classes }) => {
   document.title = '上智大学エレクトロニクスラボ';
   return (
     <div className={classes.root}>
-      <Typography variant={"overline"}>INTRODUCING</Typography>
+      <Typography variant={"overline"} className={classes.textGreen}>
+        INTRODUCING
+      </Typography>
       <Typography weight={"bold"} variant={"h4"} gutterBottom>
         上智大学 エレクトロニクスラボ
-    </Typography>
+      </Typography>
+      <img src="/assets/images/デカテスラ製作機_190403_0036.jpg" alt="テスラコイル" className={classes.indexImage}/>
+
+      <div className={classes.container}>
+      <Typography weight={"bold"} variant={"h5"} gutterBottom>
+        「理系」サークルのエレラボ
+      </Typography>
       <Typography indent={"small"}>
         我々は2018年5月23日に結成し、現在では上智大学の理工学部や経済学部の学生を中心として、様々な大学の人が集まり、活動をしています。
-    </Typography>
+      </Typography>
+      </div>
+
+      <hr/>
+
+      <div className={classes.container}>
+      <Typography weight={"bold"} variant={"h5"} gutterBottom>
+        活動内容
+      </Typography>
+
+      <div className={classes.cardArea}>
+      <Card className={classes.card}>
+      <CardActionArea>
+        
+        <CardMedia
+          className={classes.media}
+          image="/assets/images/デカテスラ製作機_190403_0036.jpg"
+          title="電子工作班"
+        />
+        <CardContent style={{ color: "#fff", backgroundColor: "#444" }}>
+          <Typography gutterBottom variant="h5" component="h2">
+            電子工作班
+            <ArrowForwardIosIcon className={classes.arrow}/>
+          </Typography>
+          <Typography variant="body2" component="p" className={classes.cardSubText}>
+            ハンダに艶を出すのがプロ
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      </Card>
+
+      <Card className={classes.card}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image="/assets/images/デカテスラ製作機_190403_0036.jpg"
+          title="プログラミング班"
+        />
+        <CardContent style={{ color: "#fff", backgroundColor: "#444" }}>
+          <Typography gutterBottom variant="h5" component="h2">
+            プログラミング班
+            <ArrowForwardIosIcon className={classes.arrow}/>
+          </Typography>
+          <Typography variant="body2" component="p" className={classes.cardSubText}>
+            Djangoをすこれ
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      </Card>
+      </div>
+      </div>
+
+      <hr />
+
+      <div className={classes.container}>
+      <Typography weight={"bold"} variant={"h5"} gutterBottom>
+        年間イベント
+      </Typography>
+      <img src="/assets/images/図1.png" alt="テスラコイル"/>
+      </div>
+
+      <hr />
+
+      <div className={classes.container}>
+      ツイッター埋め込み？
+      </div>
+
     </div>
   )
 };
